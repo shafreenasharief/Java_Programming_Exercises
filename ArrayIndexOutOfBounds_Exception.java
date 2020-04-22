9a34faed2b4ecf00e4b611b7f379832e930d69bd
shafreenasharief <53473425+shafreenasharief@users.noreply.github.com>
2020-04-22 13:40:28 +0530

    Create ArrayIndexOutOfBounds_Exception.java

---
+++ ArrayIndexOutOfBounds_Exception.java
@@ -0,0 +1,17 @@
+public class ArrayIndexOutOfBoundException {  
+  
+    public static void main(String[] args) {  
+String[] arr = {"Rohit","Shikar","Virat","Dhoni"};   
+                                       
+          
+        for(int i=0;i<=arr.length;i++) {       
+  
+
+             System.out.println(arr[i]);      
+          
+        }  
+  
+    }  
+  
+}  
+© 2020 GitHub, Inc.
